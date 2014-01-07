120b1fec293f6e6d91e645594274ce30053d0dc8
Frosty Tibbers <sstickl@clemson.edu>
2014-01-06 20:45:27 -0500

    Agenda Update
    Updated the upcoming events to use the Google Agenda feature.

--- pages/index.php
+++ pages/index.php
@@ -1,12 +1,12 @@
 <center>
+<h2>The official stream</h2>
+<object type="application/x-shockwave-flash" height="490" width="872" id="live_embed_player_flash" data="http://www.twitch.tv/widgets/live_embed_player.swf?channel=clemson_esports" bgcolor="#000000"><param name="allowFullScreen" value="true" /><param name="allowScriptAccess" value="always" /><param name="allowNetworking" value="all" /><param name="movie" value="http://www.twitch.tv/widgets/live_embed_player.swf" /><param name="flashvars" value="hostname=www.twitch.tv&channel=clemson_esports&auto_play=true&start_volume=25" /></object>
 <div class="row">
 	<div class="col-md-3">
 	</div>
 	<div class="col-md-6">
 			<h2>Upcoming events:</h2> 
-			<h3 class="featurette-heading">Spring 2014: <span class="text-muted">Clemson Championship Series Season 1</span></h2>
+			<iframe src="https://www.google.com/calendar/embed?src=culol%40g.clemson.edu&showTitle=0&amp;showNav=0&amp;showDate=0&amp;showPrint=0&amp;showTabs=0&amp;showCalendars=0&amp;mode=AGENDA&amp;height=300&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;ctz=America%2FNew_York" style=" border-width:0 " width="800" height="300" frameborder="0" scrolling="no"></iframe>
 	</div>
 </div>
-<h2>The official stream</h2>
-<object type="application/x-shockwave-flash" height="490" width="872" id="live_embed_player_flash" data="http://www.twitch.tv/widgets/live_embed_player.swf?channel=clemson_esports" bgcolor="#000000"><param name="allowFullScreen" value="true" /><param name="allowScriptAccess" value="always" /><param name="allowNetworking" value="all" /><param name="movie" value="http://www.twitch.tv/widgets/live_embed_player.swf" /><param name="flashvars" value="hostname=www.twitch.tv&channel=clemson_esports&auto_play=true&start_volume=25" /></object>
 </center>
