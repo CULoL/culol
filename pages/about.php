3925f7980a6512ed36c33209fb87ab91d8ec4afe
Frosty Tibbers <sstickl@clemson.edu>
2014-01-06 20:11:12 -0500

    About Us Update
    Update on the styling of the about us page.   Added descriptions for
    club officers.  Reworded line about official recognition from CUSG and
    RG.

--- pages/about.php
+++ pages/about.php
@@ -1,22 +1,32 @@
-<h1>About Us</h1>
-<img src="img/clubpic.png" alt="CCS" width="250px"/></center>
-<p><a href="http://http://na.leagueoflegends.com/">League of Legends</a> is a massive online battle arena (MOBA) game with over 12 million players 
+<div class="row">
+	<div class="col-md-2">
+		<h1>About Us</h1>
+		<img src="img/clubpic.png" alt="CCS" width="100%"/></br>
+		<p>From left to right: Vice President Will "Jewlittle" Craig, President Bradley "Beezleblob" Kramer, Event Organizer Chuck Wilson, and Secretary Connor "lonelybutter" Pickard.</p>
+	</div>
+	<div class="col-md-10">
+		<h1>The Clemson League of Legends Club</h1>
+		<h2>Established in April 2013</h2>
+		<p><a href="http://http://na.leagueoflegends.com/">League of Legends</a> is a massive online battle arena (MOBA) game with over 12 million players 
 
-worldwide. The Clemson University League of Legends Club is committed to providing both a fun and engaging community for players at Clemson University 
+		worldwide. The Clemson University League of Legends Club is committed to providing both a fun and engaging community for players at Clemson University 
 
-where players can share their love of the game, as well as to provide a competitive team for Clemson University with the purpose of competing against other universities
+		where players can share their love of the game, as well as to provide a competitive team for Clemson University with the purpose of competing against other universities
 
-and amateur teams in collegiate leagues and tournaments.</p>
+		and amateur teams in collegiate leagues and tournaments.</p>
 
-<p>The club regularly meets every other Wednesday, usually on campus at 8:00PM in Kinard G01 or 301.  Meetings are held
+		<p>The club regularly meets every other Wednesday, usually on campus at 8:00PM in Kinard G01 or 301.  Meetings are held
 
-to discuss upcoming and past events as well as to provide a time for players to play with each other.</p>
+		to discuss upcoming and past events as well as to provide a time for players to play with each other.</p>
 
-<p>CULoL holds many special events such as tournament viewing parties, LAN parties, and Riot-sponsored tournaments, for more information view our calender!</p>
+		<p>CULoL holds many special events such as tournament viewing parties, LAN parties, and Riot-sponsored tournaments, for more information view our calender!</p>
 
-<p>Our club is officially recognized by both Clemson University and Riot Games!</p>
+		<p>Our club is also officially recognized by both the Clemson University Student Government and Riot Games.</p>
+		
+		<p><a style="color: #FFFFFF;" class="btn btn-lg btn-success" href="page.php?page=constitution.php">Constitution</a></p>
+	</div>
+</div>
 
-<p><a style="color: #FFFFFF;" class="btn btn-lg btn-success" href="page.php?page=constitution.php">Constitution</a></p>
 <div class="row">
 	<div class="col-md-6">
 		<h2>Resources</h2>
