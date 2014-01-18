f81962ebfd9b82db20759558b4fbbb399b0a6398
Frosty Tibbers <sstickl@clemson.edu>
2014-01-18 16:22:02 -0500

    Front page centering

--- pages/index.php
+++ pages/index.php
@@ -1,10 +1,14 @@
 <center>
 <h2>The official stream</h2>
 <object type="application/x-shockwave-flash" height="490" width="872" id="live_embed_player_flash" data="http://www.twitch.tv/widgets/live_embed_player.swf?channel=clemson_esports" bgcolor="#000000"><param name="allowFullScreen" value="true" /><param name="allowScriptAccess" value="always" /><param name="allowNetworking" value="all" /><param name="movie" value="http://www.twitch.tv/widgets/live_embed_player.swf" /><param name="flashvars" value="hostname=www.twitch.tv&channel=clemson_esports&auto_play=true&start_volume=25" /></object>
-<div class="col-md-3" style="margin:0px auto;">
-	<a href="https://www.facebook.com/ClemsonUniversityLeagueOfLegendsClub"><img src="img/fb.png" alt="Facebook"/></a>
-	<a href="https://www.facebook.com/groups/414257432000301/"><img src="img/fbg.png" alt="Facebook Group"/></a>
-	<a href="https://twitter.com/clemsonleague"><img src="img/tw.png" alt="Twitter"/></a>
+<div class="row">
+	<div class="col-md-3">
+	</div>
+	<div class="col-md-6" style="margin-left: auto; margin-right: auto;">
+		<a href="https://www.facebook.com/ClemsonUniversityLeagueOfLegendsClub"><img src="img/fb.png" alt="Facebook"/></a>
+		<a href="https://www.facebook.com/groups/414257432000301/"><img src="img/fbg.png" alt="Facebook Group"/></a>
+		<a href="https://twitter.com/clemsonleague"><img src="img/tw.png" alt="Twitter"/></a>
+	</div>
 </div>
 <div class="row">
 	<div class="col-md-3">
