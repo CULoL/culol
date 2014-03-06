5e2ba09c69c83d63c9437befb3406f41c9340343
Elliot Stickle <sstickl@clemson.edu>
2014-03-05 21:56:13 -0500

    Stopped game tracking for CCS
    Nobody was sending results like I asked...

--- pages/css.php
+++ pages/css.php
@@ -8,7 +8,7 @@
     <div class="col-lg-6">
 		<h2>Structure</h2>
         <h4>Season Stage</h4>
-		<p>A stage with games spread out over 12 weeks, where all 9 teams play each other twice.
+		<p>A stage with games spread out over 12 weeks, where all 7 teams play each other twice.
 		<h4>Final Tournament</h4>
 		<p>This is a standard bracket tournament, best of three with the finals being best of five, exactly the way we saw the Season 3 finals. This tournament will be held over a three week period, mostly on weekends, which is subject to change due to the approaching finals week.</p>
     </div>
@@ -24,30 +24,20 @@
 	</div>
 </div>
 
-<h2>Stats Posters:</h2>
-<a href="img/cs/ccs%20w1.png"><img src="img/cs/ccs%20w1.png" alt="CCS Week 1" style="width: 200px;"/></a>
-<h4>Week 1</h4>
-
 <h2>The teams:</h2>
-<h4>Last Update: Week 2 Day 4</h4>
-<h6>2014-01-23 @ 10:18 PM</h6>
 <div class="table-responsive">
 	<table class="table table-striped">
 		<thead>  
 			<tr>
-				<th>Place</th>
 				<th>Team Logo</th>  
 				<th>Team Name</th>  
-				<th>W/L</th>
 				<th>Roster</th>
 			</tr>  
 		</thead>  
-		<tbody>  >	
+		<tbody>
 			<tr>   
-				<td><h1>1</h1></td>
 				<td><img src="img/cs/frakthepiltovers.png" style="max-height: 100px;"/></td>  
 				<td><h3>Frak the Piltovers [fupilt]</h3></td>  
-				<td><h1>(4-0)</h1></td>  
 				<td><a href="http://www.lolking.net/summoner/na/31533356">WashableCoyote</a>,
 					<a href="http://www.lolking.net/summoner/na/22217505">AleskiMaciej</a>,
 					<a href="http://www.lolking.net/summoner/na/19813093">Pet Troop</a>,
@@ -56,10 +46,8 @@
 				</td>
 			</tr>	
 			<tr>   
-				<td><h1>2</h1></td>
 				<td><img src="img/cs/TRU.png" style="max-height: 100px;"/></td>  
 				<td><h3>Suk R Us [SRU]</h3></td> 
-				<td><h1>(3-0)</h1></td> 
 				<td><a href="http://www.lolking.net/summoner/na/19795477">Erilur</a>, 
 					<a href="http://www.lolking.net/summoner/na/20413044">Blayer</a>, 
 					<a href="http://www.lolking.net/summoner/na/20167511">A Blind Monk</a>, 
@@ -68,10 +56,8 @@
 				</td>
 			</tr>			
 			<tr>   
-				<td><h1>3</h1></td>
 				<td><img src="img/cs/SeaLG.jpg" style="max-height: 100px;"/></td>  
 				<td><h3>SeaLG</h3></td>  
-				<td><h1>(2-0)</h1></td>
 				<td><a href="http://www.lolking.net/summoner/na/26732365">Proseidon</a>, 
 					<a href="http://www.lolking.net/summoner/na/23554379">Pray Im AFK</a>, 
 					<a href="http://www.lolking.net/summoner/na/23558994">Camster</a>, 
@@ -80,10 +66,8 @@
 				</td>
 			</tr>
 			<tr>   
-				<td><h1>4</h1></td>
 				<td><img src="img/cs/culol.png" style="max-height: 100px;"/></td>  
 				<td><h3>hybrix</h3></td>  
-				<td><h1>(3-1)</h1></td>  
 				<td><a href="http://www.lolking.net/summoner/na/35743194">shldorc</a>,
 					<a href="http://www.lolking.net/summoner/na/21619509">king stein III</a>,
 					<a href="http://www.lolking.net/summoner/na/21183965">snapdragonz</a>, 
@@ -91,11 +75,9 @@
 					<a href="http://www.lolking.net/summoner/na/23772630">superflyyyyy</a>
 				</td>
 			</tr			
-			<tr>   
-				<td><h1>5</h1></td>
+			<tr>
 				<td><img src="img/cs/SO.png" style="max-height: 100px;"/></td>  
 				<td><h3>Sicilian Offense [SO]</h3></td>  
-				<td><h1>(1-2)</h1></td>
 				<td><a href="http://www.lolking.net/summoner/na/40038802">fearmaster93</a>, 
 					<a href="http://www.lolking.net/summoner/na/20821868">drkarorkian</a>, 
 					<a href="http://www.lolking.net/summoner/na/40499558">stickx24</a>, 
@@ -104,52 +86,24 @@
 				</td>
 			</tr>	
 			<tr> 
-				<td><h1>5</h1></td>
 				<td><img src="img/cs/The Bearded Staggs.png" style="max-height: 100px;"/></td>  
 				<td><h3>The Bearded Staggs [tBS]</h3></td>  
-				<td><h1>(1-2)</h1></td>
 				<td><a href="http://www.lolking.net/summoner/na/20747613">Infernalis</a>, 
 					<a href="http://www.lolking.net/summoner/na/23081317">Noxidile</a>, 
 					<a href="http://www.lolking.net/summoner/na/21237898">GI Pritchard</a>, 
 					<a href="http://www.lolking.net/summoner/na/32157787">Xxehanort</a>, and 
 					<a href="http://www.lolking.net/summoner/na/42026881">GomerUSMC</a>
 				</td>
-			</tr>			
-			<tr>   
-				<td><h1>7</h1></td>
-				<td><img src="img/cs/NM.png" style="max-height: 100px;"/></td>  
-				<td><h3>New Meta [NM]</h3><p><i>Not following standard Meta positions</i></p></td>  
-				<td><h1>(1-3)</h1></td> 
-				<td><a href="http://www.lolking.net/summoner/na/19966328">CaptainBanHammer</a>, 
-					<a href="http://www.lolking.net/summoner/na/29481736">tozzid</a>, 
-					<a href="http://www.lolking.net/summoner/na/40600313">Trustmeengineer</a>, 
-					<a href="http://www.lolking.net/summoner/na/25408538">Dont taze me sis</a>, and 
-					<a href="http://www.lolking.net/summoner/na/33320877">Mcbadfish</a>
-				</td>
-			</tr>
+			</tr>		
 			<tr>  
-				<td><h1>8</h1></td>
 				<td><img src="img/cs/TWD.jpg" style="max-height: 100px;"/></td>  
 				<td><h3>Texting While Draven [TWD]</h3></td>  
-				<td><h1>(0-2)</h1></td>
 				<td><a href="http://www.lolking.net/summoner/na/37909089">phantomcash</a>, 
 					<a href="http://www.lolking.net/summoner/na/20760518">CowInAPie</a>, 
 					<a href="http://www.lolking.net/summoner/na/19055924">Thunderhawk1989</a>, 
 					<a href="http://www.lolking.net/summoner/na/19039926">madman258</a>, and 
 					<a href="http://www.lolking.net/summoner/na/21550724">Thutmose I</a>
 				</td>  
-			</tr>
-			<tr>   
-				<td><h1>9</h1></td>
-				<td><img src="img/cs/LS.png" style="max-height: 100px;"/></td>  
-				<td><h3>LOLI SMASHER [LS]</h3></td>  
-				<td><h1>(0-4)</h1></td> 
-				<td><a href="http://www.lolking.net/summoner/na/31632441">LGE DABOMB</a>, 
-					<a href="http://www.lolking.net/summoner/na/31549877">Czar Turtle III</a>, 
-					<a href="http://www.lolking.net/summoner/na/25729678">pandanihao</a>, 
-					<a href="http://www.lolking.net/summoner/na/40952157">Quanday3000</a>, and 
-					<a href="http://www.lolking.net/summoner/na/27344011">sighted</a>
-				</td> 
 			</tr>
 		</tbody>
 	</table>
