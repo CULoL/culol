addebeee4d23920d5de4f8948a64a3635443a35c
Elliot Stickle <sstickl@clemson.edu>
2014-03-05 21:55:33 -0500

    CCS Rules

---
+++ pages/ccsrules.php
@@ -0,0 +1,31 @@
+<h1>CLEMSON UNIVERSITY LEAGUE OF LEGENDS CLUB</h1>
+<h1>Clemson Collegiate Series Season 1 Rules</h1>
+<center><img src="img/cs2.png" alt="CCS" width="250px"/></center>
+
+<h2 class="featurette-heading">Match Rules</h2>
+<p><span class="text-muted">Match Rescheduling</span>&nbsp;&nbsp;&nbsp;&nbsp;A day's prior notice is required to miss any match with no penalty. 
+	Failure to comply results in a forfeit for your team for the match, and missing two matches will see your team disqualified from the tournament. 
+	This will be reviewed on a case-by-case basis to try to catch any special cases.  Any match that is missed with prior notice will be rescheduled.</p>
+<p><span class="text-muted">Match Start</span>&nbsp;&nbsp;&nbsp;&nbsp;Check in ends 15 minutes after start time. Failure for an entire team (5 summoners) to report in before this time results in a forfeit.</p>
+<p><span class="text-muted">Substitutes</span>&nbsp;&nbsp;&nbsp;&nbsp;Substitutes may be brought in before the match starts with prior notice to both the coordinator (Bradley Kramer), the webmaster (Shawn Stickle), and the opposing team captain.
+	Substitutes may not be brought in after the start time. So if your team isn't responding to you before the match, go ahead and have a sub lined up.</p>
+<p><span class="text-muted">Pauses</span>&nbsp;&nbsp;&nbsp;&nbsp;Teams are limited to two pauses per game totaling no more than 5 minutes. Any longer results in a forfeit for the match.</p>
+<p><span class="text-muted">Loading Problems</span>&nbsp;&nbsp;&nbsp;&nbsp;If the game or a player has any issues loading or with disconnects, the game may be restarted up to a point. Constant failure to connect may result in a forfeit of the match.</p>
+<p><span class="text-muted">Other potential DCs</span>&nbsp;&nbsp;&nbsp;&nbsp;If a player disconnects for any other reason (read: rage quits) the game will progress as usual.</p>
+<p><span class="text-muted">Streaming</span>&nbsp;&nbsp;&nbsp;&nbsp;Every game will be streamed with casters.</p>
+
+<h2 class="featurette-heading">Series Disqualification</h2>
+<p>Disqualification from the entire Series can result from any of the following at the coordinator's discretion:</p>
+<p><span class="text-muted">Cheating</span>&nbsp;&nbsp;&nbsp;&nbsp;Any team accused and proved to be using any hacks, bots, or other forms of cheating.</p>
+<p><span class="text-muted">Failure to follow the Summoners Code</span>&nbsp;&nbsp;&nbsp;&nbsp;Any team displaying poor sportsmanship or not following the Summoners Code.</p>
+<p><span class="text-muted">Account Sharing</span>&nbsp;&nbsp;&nbsp;&nbsp;Summoners using the original rosters summoner name with a different player playing on the account.</p>
+<p><span class="text-muted">Throwing</span>&nbsp;&nbsp;&nbsp;&nbsp;Intentionally Feeding/Throwing the game for the purpose of the other team winning.</p>
+<br/>
+<br/>
+<p>If you have any questions or concerns about the rule, or if you need the email(s) to the coordinator, webmaster, or a team captain, please contact us at culol@clemson.edu.</p>
+
+    
+    
+    
+    
+    
